4c0e2341df8ae0b833b9a08ee1f65332a813238b
AryanBhati7 <codiearyan07@gmail.com>
2024-11-10 04:37:41 +0530

    authsatus added in store, alert logic in navbar

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { store } from './store/store'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.tsx'
-import { Home } from "./pages"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
+import { Home } from "./pages";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { PUBLISHABLE_KEY } from "../constants.ts";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -15,13 +17,20 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home />,
       },
+      // {
+      //   path: "/volunteer",
+      //   element: <Volunteer />,
+      // },
     ],
   },
 ]);
-createRoot(document.getElementById('root')!).render(
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </StrictMode>,
-)
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </ClerkProvider>
+  </StrictMode>
+);
